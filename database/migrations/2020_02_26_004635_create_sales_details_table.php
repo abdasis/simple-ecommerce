21c2e5c825af657243aff497f1090118a17a032f
Abd. Asis <id.juniordeveloper@gmail.com>
2020-02-26 08:29:30 +0700

    first commit

---
+++ database/migrations/2020_02_26_004635_create_sales_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('sales_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('qty');
+            $table->timestamps();
+            $table->foreign('sales_id')->references('id')->on('sales')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales_details');
+    }
+}
---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,40 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html">Stisla</a>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">St</a>
+      </div>
+      <ul class="sidebar-menu">
+        <li class="menu-header">Dashboard</li>
+
+        <li><a href="#" class="nav-link"><i class="fas fa-columns"></i> <span>Dashboard</span></a></li>
+
+        {{-- menu product --}}
+        <li class="dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-box-open"></i> <span>Product</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="layout-default.html">Tambah Product</a></li>
+            <li><a class="nav-link" href="layout-transparent.html">Daftar Product</a></li>
+          </ul>
+        </li>
+
+        {{-- menu store --}}
+        <li class="dropdown">
+            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-store"></i> <span>Store</span></a>
+            <ul class="dropdown-menu">
+            <li><a class="nav-link" href="layout-default.html">Tambah Store</a></li>
+            <li><a class="nav-link" href="layout-transparent.html">Daftar Store</a></li>
+            </ul>
+        </li>
+
+        
+        
+
+      <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+        <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+          <i class="fas fa-rocket"></i> Documentation
+        </a>
+      </div>        </aside>
+  </div>
